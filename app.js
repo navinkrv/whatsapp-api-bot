444e4de824eacd17bc6f2ddb36741640d76bec4a
navin kumar <navinkrv@gmail.com>
2023-12-22 12:12:45 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const qrcode = require("qrcode-terminal");
+
+const { Client } = require("whatsapp-web.js");
+
+const app = express();
+const client = new Client();
+let qrCode;
+client.on("qr", (qr) => {
+	qrcode.generate(qr, { small: true });
+	// console.log(qr);
+	qrCode = qr;
+});
+
+client.on("ready", () => {
+	console.log("Client is ready!");
+});
+
+client.initialize();
+
+// API
+
+app.get("/login", (req, res) => {
+	res.send(qrCode);
+	// console.log(qrCode);
+});
+
+app.get("/send/:num/:text", (req, res) => {
+	// Number where you want to send the message.
+	const number = req.params.num;
+
+	// Your message.
+	const text = req.params.text;
+
+	// Getting chatId from the number.
+	// we have to delete "+" from the beginning and add "@c.us" at the end of the number.
+	const chatId = number + "@c.us";
+
+	// Sending message.
+	if (client.sendMessage(chatId, text)) {
+		res.json({
+			msg: "sent",
+			data: chatId + " " + text
+		});
+	} else {
+		res.json({
+			msg: " not sent"
+		});
+	}
+});
+
+app.listen(5000, () => {
+	console.log("server running at port 5000");
+});
